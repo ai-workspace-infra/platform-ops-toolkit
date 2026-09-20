6e58c1d4ca28dbcb55508b96cc5f437213e5530c
Haitao Pan <shenlan@HaitaodeMacBook-Pro.local>
2026-09-20 13:11:41 +0800

    fix(terraform): rely on rendered S3 endpoint

--- .github/scripts/platform-ops/provision/platform-ops_provision_terraform-init.sh
+++ .github/scripts/platform-ops/provision/platform-ops_provision_terraform-init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 terraform init -input=false \
-  -backend-config="endpoints.s3=${TF_STATE_ENDPOINT}" \
   -backend-config="bucket=${TF_STATE_BUCKET}" \
   -backend-config="key=${ENV_STEPS_ROUTE_OUTPUTS_STATE_KEY}" \
   -backend-config="access_key=${TF_STATE_ACCESS_KEY}" \
